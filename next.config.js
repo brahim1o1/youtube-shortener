30131fe227423bc3a0b47ae20b14b9fb3d7786f6
brahim labhih <labhihbrahim@hotmail.com>
2024-11-14 03:35:41 -0800

    fix some errors

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['img.youtube.com', 'i.ytimg.com'], // Add any other YouTube domains you need
+    }
+  }
+  
+  module.exports = nextConfig
